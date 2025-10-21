8fab0019cfcfb50f0b2d3ebde17b82532a537eba
Dee-Des <desmonddee2014@gmail.com>
2025-10-21 12:55:28 +0100

    Add functionality to get and display display from an API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
 console.log("Connected to script.js");
+
+//Get data from PokeAPI about Characters
+
+//fetchData();
+
+async function fetchData() {
+
+    try {
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok) {
+            throw new Error ("Could not fetch resource");
+        }
+
+        const data = await response.json();
+
+        //to log data returning to the console
+        //console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+}
