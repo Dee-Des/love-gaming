1f1d4cade7f5308db84e60d2bc0ba4fb8dc2bbe8
Dee-Des <desmonddee2014@gmail.com>
2025-10-21 16:39:12 +0100

    Add amendments to EventListeners in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ console.log("Connected to script.js");
 //Get the button elements and add event listeners to them
 
 
-document.addEventListener("COMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
