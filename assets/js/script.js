f5fc284e562fb2346edfba75a2d3ea755d852c7a
Dee-Des <desmonddee2014@gmail.com>
2025-10-21 20:57:47 +0100

    Add Event Listeners for document laoding and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
 console.log("Connected to script.js");
 
+
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+
+document.addEventListener("COMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "sumit") {
+                alert("You clicked Submit!");
+            }else {
+                let gameType = this .getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+
+});
 //Get data from PokeAPI about Characters
 
 //fetchData();
