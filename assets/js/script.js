b306f7f0f72a2f92f7a6844c5658aea57d0ca91f
Dee-Des <desmonddee2014@gmail.com>
2025-10-21 20:06:19 +0100

    Reverted project back to Fetch a Pokemon with initial folder structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,5 @@
 console.log("Connected to script.js");
 
-
-//Wait for the DOM to finish loading before running the game
-//Get the button elements and add event listeners to them
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
-            }else {
-                let gameType = this .getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
-                runGame(gameType);
-            }
-        })
-    }
-
-});
-
-runGame("addition");
-
-/**
- * * The main game "loop", called when the script is first loaded
- * * and after the user's answer has been processed
- */
-function runGame(gameType) {
-
-
-    //Creates two random numbers between 1 and 25
-    //let num1 = Math.floor(Math.random() * 25 + 1);
-    //let num = Math.floor(Math.random() * 25 + 1);
-
-        if (gameType ==="addition") {
-            displayAdditionQuestion(num1, num2);
-        } else {
-            alert(`Unknown game type: ${gameType}`);
-            throw `Unknown game type: ${gameType}. Aborting!`
-        }
-    }
-
-function checkAnswer() {
-
-}
-
-
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore() {
-}
-
-function incrementWrongAnswer() {
-
-}
-
-function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand').textContent = operand2;
-    document.getElementById('operator').textContent = "+";
-
-
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
-
-}
-
-
-
 //Get data from PokeAPI about Characters
 
 //fetchData();
@@ -85,7 +8,7 @@ async function fetchData() {
 
     try {
 
-        const pokemonName = document.getElementById("answer-box-pokemonName").value.toLowerCase();
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok) {
@@ -105,4 +28,3 @@ async function fetchData() {
         console.error(error);
     }
 }
-
