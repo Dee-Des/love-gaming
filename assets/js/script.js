ba640c9f415a98d278b4233c4d3941f7e29ca06d
Dee-Des <desmonddee2014@gmail.com>
2025-10-21 15:21:49 +0100

    Add initial html structure to controls area

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ async function fetchData() {
 
     try {
 
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const pokemonName = document.getElementById("answer-box-pokemonName").value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok) {
